23b01556f0db4079ff3768a34b49e1c08346b5de
Ubuntu <adithyazure@Slide-in-the-VM.tiv0r0se415upmft4042d5jvdc.bx.internal.cloudapp.net>
2021-09-21 10:34:03 +0000

    initialized README.md with few instructions regarding installation

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# project1-avenkatesh1
+![image text]("https://cdn.freebiesupply.com/logos/large/2x/spotify-2-logo-png-transparent.png")
+
+# Spotify Genius Music Player
 Spotify track app that utilizes Spotify and Genius Lyrics API to render our application in a website browser.
+
+## Instructions
+
+To run this app on your local machine, clone this repository using the command ```git clone https://github.com/csc4350-f21/project1-avenaktesh1.git``` to the specified directory you want the project to be copied in.
+
+
+### API's
+This web application utilizes Spotify API and Genius lyrics API. More information found here about these sources:
+- [Getting started with SPotify API]("")
+- [Getting started with Genius API]("https://docs.genius.com/#/getting-started-h1")
+
+
+
+### Installations
+This web application uses Flask for backend processes and Heroku for deployment.
+- Flask
--- genuis.py
+++ genuis.py
@@ -4,7 +4,30 @@ import os
 import random
 from dotenv import find_dotenv, load_dotenv
 
-load_dotenv(find_dotenv)
+# Environment variables
+load_dotenv(find_dotenv())
 
+# Client ID and Client Secret from the Genius web dashboard portal from .env
 CLIENT_ID = os.getenv('CLIENT_ID_genius')
-CLIENT_SECRET = os.getenv
+CLIENT_SECRET = os.getenv('CLIENT_SECRET_genius')
+
+# Client Credentials Flow Authorization URL
+AUTH_URL = "https://api.genius.com/oauth/token"
+
+# Post request to generate authorization token
+auth_response = requests.post(AUTH_URL, {
+    "grant_type": "client_credentials",
+    "client_id": CLIENT_ID,
+    "client_secret": CLIENT_SECRET
+})
+
+# Jsonify authorization response
+auth_response_data = auth_response.json()
+
+# Acces the 'access_token' credentials from the JSON object
+access_token = auth_response_data['access_token']
+
+# Import the auth token into the Authorization key header
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
