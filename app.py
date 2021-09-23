84db9f378a5c67204aa22184104ce19abf6e42b6
Ubuntu <adithyazure@Slide-in-the-VM.tiv0r0se415upmft4042d5jvdc.bx.internal.cloudapp.net>
2021-09-23 01:28:35 +0000

    heroku changes

--- app.py
+++ app.py
@@ -12,5 +12,8 @@ app = flask.Flask(__name__)
 def main():
     return flask.render_template("index.html", data = data, genius_url = genius_url)
 
-app.run(host='0.0.0.0',port=int("PORT",8080))
+app.run(
+    host='0.0.0.0',
+    port=int("PORT",8080)
+)
 # app.run()
