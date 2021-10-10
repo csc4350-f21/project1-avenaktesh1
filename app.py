17089e9aa73a4b10b58933e57a58bc85865bed92
Ubuntu <adithyazure@Slide-in-the-VM.tiv0r0se415upmft4042d5jvdc.bx.internal.cloudapp.net>
2021-10-10 02:12:50 +0000

    import SQLAlchemy and code ocunterparts to initialize db

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import flask
 import os
 from spotify import get_spotify_data, get_genius_data
 from dotenv import find_dotenv, load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 
 # Environment variables
 load_dotenv(find_dotenv())
@@ -11,6 +12,15 @@ data = get_spotify_data()
 genius_url = get_genius_data()[0]
 
 app = flask.Flask(__name__)
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(120))
+    due_date = db.Column(db.String(80))
+
+db.create_all()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 
