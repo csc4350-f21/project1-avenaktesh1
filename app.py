cf566c2c6bb47762b019a9783d31314b420628c0
Ubuntu <adithyazure@Slide-in-the-VM.tiv0r0se415upmft4042d5jvdc.bx.internal.cloudapp.net>
2021-10-10 00:14:59 +0000

    sqlalchemy-database-uri created

--- app.py
+++ app.py
@@ -2,11 +2,20 @@ from os import lseek
 import flask
 import os
 from spotify import get_spotify_data, get_genius_data
+from dotenv import find_dotenv, load_dotenv
+
+# Environment variables
+load_dotenv(find_dotenv())
 
 data = get_spotify_data()
 genius_url = get_genius_data()[0]
 
 app = flask.Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+
+# Suppres a warning - not strictly needed
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.route("/")
 def main():
