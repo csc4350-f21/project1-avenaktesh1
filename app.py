3cf809b0f260d62001788168a4a86eb16ec06aa7
Ubuntu <adithyazure@Slide-in-the-VM.tiv0r0se415upmft4042d5jvdc.bx.internal.cloudapp.net>
2021-10-14 05:12:51 +0000

    flask login authentication work

--- app.py
+++ app.py
@@ -1,32 +1,42 @@
 from os import lseek
 import flask
 import os
+
+from flask_login.utils import logout_user
+from werkzeug.utils import redirect
 from spotify import get_spotify_artist_info
 from dotenv import find_dotenv, load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, login_required, current_user
 
 # Environment variables
 load_dotenv(find_dotenv())
 
-# create flask app 
+# Create flask app 
 app = flask.Flask(__name__)
 
-# intialize SQL Alchemy Database
+# Intialize SQL Alchemy Database
 db = SQLAlchemy(app)
-app.debug = True
 
-# replace the 'postgres' uri to 'postgresql'
+# Login Manager Instantiated
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+
+# Replace the 'postgres' uri to 'postgresql'
 uri = os.getenv('SQLALCHEMY_DATABASE_URI')
 if uri.startswith('postgres://'):
     uri = uri.replace("postgres://", 'postgresql://', 1)
 
-# set the uri to the app.config
+# Set the uri to the app.config
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
+app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
 
 # Suppress a warning - not strictly needed
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# create the database tables
+# Create the database tables
 db.create_all()
 
 # Register Page (starting page)
@@ -34,15 +44,44 @@ db.create_all()
 def register():
    if flask.request.method == "POST":
        username__register = flask.request.form.get('username__register')
-       db.session.add(username__register)
+       username__DB_entry = Username(username=username__register)
+       db.session.add(username__DB_entry)
        db.session.commit()
 
    return flask.render_template("register.html")
 
+# Flask Login
+@login_manager.user_loader
+def load_user(username_id):
+    return Username.query.get(int(username_id))
+
 # Login Page (input user into database)
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if flask.request.method == "POST":
+        username__login = flask.request.form.get('username__login')
+
+        username__DB_login = Username.query.filter_by(username = username__login).first()
+        print(username__DB_login)
+
+        if username__DB_login:
+            login_user(username__DB_login)
+            return flask.redirect('/index')
+        else:
+            print("Error please register an account")
     return flask.render_template('login.html')
+
+# Flask Logout
+app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return "logged out"
+
+app.route('/home')
+@login_required
+def home():
+    return 'current user is' + current_user.username
 
 # Index Page (app page)
 @app.route('/index', methods=['GET','POST'])
@@ -52,6 +91,8 @@ def index():
 if __name__ == "__main__":
     from models import Username, Artist_ID
     app.run(
+        # Debug mode
+        debug = True,
         host= '0.0.0.0',
         port= int(os.getenv("PORT", 8080)),
     )
--- models.py
+++ models.py
@@ -1,9 +1,10 @@
 from app import db
-class Username(db.Model):
+from flask_login import UserMixin
+class Username(db.Model, UserMixin):
    id = db.Column(db.Integer, primary_key = True)
    username = db.Column(db.String(80), unique=True)
 
-class Artist_ID(db.Model):
+class Artist_ID(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.column(db.String(80))
     artist_name = db.Column(db.String(120), unique=True)
